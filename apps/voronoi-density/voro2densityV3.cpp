66d9417145719a4413c056ee443e7f76ea7e7c58
Samuel Li <shaomenglee@gmail.com>
2018-02-23 17:31:50 -0700

    serial version works well; parallelize is the next step

--- apps/voronoi-density/voro2densityV3.cpp
+++ apps/voronoi-density/voro2densityV3.cpp
@@ -47,18 +47,25 @@ int main(int argc, char** argv )
     }
     struct timeval start, end;
 
-    // read in particles
-    long int nParticles;
+    // Read in particles
+    long nParticles;
     float*   ptcBuf = NULL;
     ReadParticles2( argv[1], nParticles, &ptcBuf );
-    long int nPtcToUse = nParticles;           // use a subset of particles for experiments
+    long nPtcToUse = nParticles;           // use a subset of particles for experiments
 
-    // each particle has a counter 
-    long int* counter = new long int[ nPtcToUse ];
+    // Each particle has a counter 
+    int* counter = new int[ nPtcToUse ];
     for( long i = 0; i < nPtcToUse; i++ )
         counter[i] = 0;
 
-    // build a kd-tree for these particles
+    // Each grid point has a pointer.
+    // This pointer points to a counter.
+    // This counter belongs to the voronoi cell this grid point resides in.
+    int** pcounter = new int*[ totalGridPts ];
+    for( long i = 0; i < totalGridPts; i++ )
+        pcounter[i] = NULL;
+
+    // Build a kd-tree for these particles
     gettimeofday( &start, NULL );
     struct kdtree* kd = kd_create(3);   
     for( long i = 0; i < nPtcToUse; i++ )
@@ -71,18 +78,20 @@ int main(int argc, char** argv )
     
     // Find the closest particle for each grid point
     gettimeofday( &start, NULL );
-    for( long int z = 0; z < GRIDZ; z++ )
+    //for( long z = 0; z < GRIDZ; z++ )
+    for( long z = 200; z < 201; z++ )
     {
-        long int zOffset = z * GRIDX * GRIDY;
-        for( long int y = 0; y < GRIDY; y++ )
+        long zOffset = z * GRIDX * GRIDY;
+        for( long y = 0; y < GRIDY; y++ )
         {
-            long int yOffset = y * GRIDX + zOffset;
-            for( long int x = 0; x < GRIDX; x++ )
+            long yOffset = y * GRIDX + zOffset;
+            for( long x = 0; x < GRIDX; x++ )
             {
                 struct kdres* set = kd_nearest3f( kd, (float)x, (float)y, (float)z );
                 assert( kd_res_size( set ) == 1 );
-                long* pt = (long*)kd_res_item_data( set );
+                int* pt = (int*)kd_res_item_data( set );
                 (*pt)++;
+                pcounter[ x+yOffset ] = pt;
                 kd_res_free( set );
             }
         }
@@ -90,18 +99,33 @@ int main(int argc, char** argv )
     gettimeofday( &end, NULL );
     std::cerr << "kdtree retrieval takes " << GetElapsedSeconds(&start, &end) << " seconds." << std::endl;
 
+/**** print diagnostic info ****/
+    // What's the total count all counters have?
+    int total = 0;
+    for( long i = 0; i < nPtcToUse; i++ )
+        total += counter[i];
+    std::cout << "Total count is : " << total << std::endl;
+
+    // How many pointers to counters are non-NULL?
+    total = 0;
+    for( int i = 0; i < totalGridPts; i++ )
+        if( pcounter[i] != NULL )
+            total++;
+    std::cout << "Total non-NULL pointer is : " << total << std::endl;
+
     // Print some random counters
     for( int i = 0; i < 10; i++ )
     {
         int idx = (float)rand() / RAND_MAX * nPtcToUse;
         std::cout << "A random counter value: " << counter[idx] << std::endl;
     }
+/**** finish printing diagnostic info ****/
 
     
-
-#if 0
-    // Each grid point calculates its own weight
-    for( long int z = 0; z < GRIDZ; z++ )
+    // Each grid point calculates its own density
+    float* density = new float[totalGridPts];
+    //for( long z = 0; z < GRIDZ; z++ )
+    for( long int z = 200; z < 201; z++ )
     {
         long int zOffset = z * GRIDX * GRIDY;
         for( long int y = 0; y < GRIDY; y++ )
@@ -109,15 +133,15 @@ int main(int argc, char** argv )
             long int yOffset = y * GRIDX + zOffset;
             for( long int x = 0; x < GRIDX; x++ )
             {
-                float  gridPt[3]     = {(float)x, (float)y, (float)z};
-                int pid              = (int)density[ x+yOffset ];
-                float myWeight       = CalcWeight( gridPt, ptcBuf + pid * 3 );
+                long   idx          = x+yOffset;
+                int    count        = *pcounter[ idx ]; 
+                density[ idx ]      = 1.0f/(float)count;
             }
         }
     }
 
     // How many Voronoi cells do not contain a grid point?
-    long int emptyCellCount = 0;
+    /* long int emptyCellCount = 0;
     for( long int i = 0; i < nPtcToUse; i++ )
         if( ptcWeight[i] == 0.0f )
         {
@@ -135,16 +159,17 @@ int main(int argc, char** argv )
             for( int j = 0; j < 3; j++ )
                 std::cerr << "Particle coord = " << particle[j] << ";  Grid coord = " << grid[j] << std::endl; 
         }
-
+    */
     
     // Output the density field
     FILE* f = fopen( argv[2], "w" );
     size_t rt = fwrite( density, sizeof(float), totalGridPts, f );
     fclose( f );
-#endif
     
-    kd_free( kd );
+    delete[] density;
+    delete[] pcounter;
     delete[] counter;
+    kd_free( kd );
     if( ptcBuf )
         delete[] ptcBuf;
 }
