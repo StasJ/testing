aa370aee57f94a62758b465edf89f98ed60bbefa
Stanislaw Jaroszynski <stasj@ucar.edu>
2019-06-20 17:01:27 -0600

    inc

--- lib/render/VolumeRenderer.cpp
+++ lib/render/VolumeRenderer.cpp
@@ -335,13 +335,11 @@ bool VolumeRenderer::_usingColorMapData() const
 void VolumeRenderer::_saveOriginalViewport()
 {
     glGetIntegerv(GL_VIEWPORT, _originalViewport);
-    printf("DVR: Saved viewport [%i, %i, %i, %i]\n", _originalViewport[0], _originalViewport[1], _originalViewport[2], _originalViewport[3]);
 }
 
 void VolumeRenderer::_restoreOriginalViewport()
 {
     glViewport(_originalViewport[0], _originalViewport[1], _originalViewport[2], _originalViewport[3]);
-    printf("DVR: Restored viewport [%i, %i, %i, %i]\n", _originalViewport[0], _originalViewport[1], _originalViewport[2], _originalViewport[3]);
 }
 
 void VolumeRenderer::_initializeFramebuffer(bool fast)
