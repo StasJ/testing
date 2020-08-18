d05fc1d3e830a19a03b6b5d04e6e3042371d9026
Scott Pearse <pearse@ucar.edu>
2020-08-18 07:40:58 -0600

    test

--- .circleci/downloadWin3rdParty.py
+++ .circleci/downloadWin3rdParty.py
@@ -16,9 +16,9 @@ with zipfile.ZipFile("2019-Aug-Win32.zip", 'r') as zip_ref:
 #
 # Download NSIS installer from google drive, until Nullsoft fixes their download certificate
 #
-url = "https://drive.google.com/a/ucar.edu/uc?id=1sRlE06jSVrCScrt546G4UtI3hj93xV2W"
+'''url = "https://drive.google.com/a/ucar.edu/uc?id=1sRlE06jSVrCScrt546G4UtI3hj93xV2W"
 #url = "https://drive.google.com/a/ucar.edu/uc?id=1YweSaUX0S08E8HEF1Mee2o5gei7WZj1a"
 output = "nsis-3.06.1-setup.exe"
 gdown.download(url, output, quiet=False)
 #os.system("./nsis-3.06.1-setup.exe")
-os.system("C:\\Users\\circleci\\project\\nsis-3.06.1-setup.exe /S /NCRC")
+os.system("C:\\Users\\circleci\\project\\nsis-3.06.1-setup.exe /S /NCRC")'''
