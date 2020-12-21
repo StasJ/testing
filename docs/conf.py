5de02a4c471889c84fd3d2a2a4199a18883682cf
Scott Pearse <pearse@ucar.edu>
2020-12-21 11:02:32 -0700

    update logo

--- docs/conf.py
+++ docs/conf.py
@@ -92,8 +92,8 @@ pygments_style = None
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-html_logo = "../_images/vaporLogoWhite2.png"
-html_favicon = "../_images/vaporVLogo.png"
+html_logo = "_images/vaporLogoWhite2.png"
+html_favicon = "_images/vaporVLogo.png"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
